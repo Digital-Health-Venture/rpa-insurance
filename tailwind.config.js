8cba713d0dc1447d340a0309ff16611f8ef7eddc
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-01-15 16:41:51 +0700

    feat: add configuration for tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,34 @@
 export default {
   darkMode: false,
   content: [],
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        avenir: ['Avenir', 'sans-serif'],
+        prompt: ['Prompt', 'sans-serif'],
+      },
+      boxShadow: {
+        s: '0px 1px 9px 0px rgba(108, 108, 108, 0.25)',
+      },
+      colors: {
+        gray: {
+          100: '#BFBFBF',
+          200: '#929292',
+          300: '#545454',
+        },
+        blue: {
+          100: '#4BAEC5',
+        },
+        red: {
+          100: '#EC3737',
+        },
+        teal: {
+          100: '#55C1BB',
+          200: '#038986',
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
