841e37789392c6df7f6698cdc9f6c4599bbb60f6
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-01-31 18:40:24 +0700

    feat(component): create scan frame

---
+++ src/components/ScanFrame.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- QR Scan Frame Corners -->
+  <div class="absolute inset-0 pointer-events-none">
+    <!-- Top-Left Corner -->
+    <div
+      class="absolute top-20 left-10 w-6 h-6 md:w-8 md:h-8 border-t-4 border-l-4 border-white"
+    ></div>
+    <!-- Top-Right Corner -->
+    <div
+      class="absolute top-20 right-10 w-6 h-6 md:w-8 md:h-8 border-t-4 border-r-4 border-white"
+    ></div>
+    <!-- Bottom-Left Corner -->
+    <div
+      class="absolute bottom-20 left-10 w-6 h-6 md:w-8 md:h-8 border-b-4 border-l-4 border-white"
+    ></div>
+    <!-- Bottom-Right Corner -->
+    <div
+      class="absolute bottom-20 right-10 w-6 h-6 md:w-8 md:h-8 border-b-4 border-r-4 border-white"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
