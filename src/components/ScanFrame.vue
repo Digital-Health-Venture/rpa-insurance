7a303ed1f89fd35951e2691371b510fd4fc1ef25
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-03 10:13:37 +0700

    style: revise size

--- src/components/ScanFrame.vue
+++ src/components/ScanFrame.vue
@@ -1,21 +1,21 @@
 <template>
   <!-- QR Scan Frame Corners -->
-  <div class="absolute inset-0 pointer-events-none">
+  <div class="absolute inset-0 pointer-events-none hidden md:flex">
     <!-- Top-Left Corner -->
     <div
-      class="absolute top-20 left-10 w-6 h-6 md:w-8 md:h-8 border-t-4 border-l-4 border-white"
+      class="absolute top-20 left-20 w-6 h-6 md:w-8 md:h-8 border-t-4 border-l-4 border-white"
     ></div>
     <!-- Top-Right Corner -->
     <div
-      class="absolute top-20 right-10 w-6 h-6 md:w-8 md:h-8 border-t-4 border-r-4 border-white"
+      class="absolute top-20 right-20 w-6 h-6 md:w-8 md:h-8 border-t-4 border-r-4 border-white"
     ></div>
     <!-- Bottom-Left Corner -->
     <div
-      class="absolute bottom-20 left-10 w-6 h-6 md:w-8 md:h-8 border-b-4 border-l-4 border-white"
+      class="absolute bottom-20 left-20 w-6 h-6 md:w-8 md:h-8 border-b-4 border-l-4 border-white"
     ></div>
     <!-- Bottom-Right Corner -->
     <div
-      class="absolute bottom-20 right-10 w-6 h-6 md:w-8 md:h-8 border-b-4 border-r-4 border-white"
+      class="absolute bottom-20 right-20 w-6 h-6 md:w-8 md:h-8 border-b-4 border-r-4 border-white"
     ></div>
   </div>
 </template>
