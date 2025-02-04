2516c302b8423c91b4a8580c51c1e6b91e07e39d
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-04 16:36:54 +0700

    chore: create const for options

---
+++ src/constants/options.ts
@@ -0,0 +1,12 @@
+import thaiLifeInsuranceLogo from '@/assets/logo/insuranceCompanyLogo/thai-life-insurance.svg';
+import muangThaiLifeLogo from '@/assets/logo/insuranceCompanyLogo/muang-thai-life.svg';
+
+export const TREATMENT_TYPE_OPTIONS = [
+  { code: '00', label: 'OPD - เจ็บป่วยทั่วไป' },
+  { code: '01', label: 'OPD - อุบัติเหตุ' },
+]
+
+export const INSURANCE_COMPANY_OPTIONS = [
+  { src: thaiLifeInsuranceLogo, label: 'ไทยประกันชีวิต' },
+  { src: muangThaiLifeLogo, label: 'เมืองไทยประกันชีวิต' },
+];
