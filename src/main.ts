c42690413fa0009948ff54c542c7280e8ed63e73
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-04 16:25:21 +0700

    feat: import vuetify

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,29 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import "./index.css";
+import './index.css'
+
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import * as labsComponents from 'vuetify/labs/components'
+
+const vuetify = createVuetify({
+  directives,
+  components: {
+    ...components,
+    ...labsComponents,
+  },
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
