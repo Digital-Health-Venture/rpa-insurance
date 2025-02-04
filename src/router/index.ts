7bd0324f3a476a4797710d13fd94c58428e1525f
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-04 16:37:37 +0700

    feat: create router for paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import { PATH_HOME, PATH_LOGIN } from '@/constants/paths'
+
+import LoginView from '@/views/LoginView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: { path: PATH_HOME },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: PATH_LOGIN,
+      name: 'loginView',
+      component: LoginView,
+      meta: { requiresAuth: false },
+    },
+    {
+      path: PATH_HOME,
+      name: 'homeView',
+      component: HomeView,
+      // meta: { requiresAuth: true },
     },
   ],
 })
