19e2d2d18bc4f906ac8108e2221da935ce9f5322
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-04 16:29:00 +0700

    feat(component): create header for info card

---
+++ src/components/infoCard/HeaderInfoCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col gap-y-2">
+    <span class="text-black font-prompt text-[18px] font-normal leading-normal"
+      >คุณ {{ name }}</span
+    >
+    <span class="text-blue-100 font-prompt text-[20px] font-medium leading-normal">{{ hn }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {},
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    hn: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
