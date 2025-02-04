a3a79d9a93e6f3d3a8ed2bc9ed6aae49852c9d3c
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-04 16:37:24 +0700

    mock: create mock data

---
+++ src/mocks/mockData.ts
@@ -0,0 +1,14 @@
+export const mockPatientData = [
+  {
+    name: 'สุXXXXXX  ติXXXXX',
+    hn: 'HN-334-324324',
+    details: {
+      '14 มิถุนายน 2567': [
+        { time: '14:00', list: 'ตรวจร่างกาย' },
+        { time: '15:00', list: 'ตรวจสุขภาพ' },
+      ],
+      '15 มิถุนายน 2567': [{ time: '08:00', list: 'งดอาหาร' }],
+    },
+    treatmentTypeCode: '00',
+  },
+]
